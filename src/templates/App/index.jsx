3625f6cb3f6fe19beee6ff2c29ed747c930ae85e
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-10-01 12:49:08 -0300

    Configurando o Jest e Jest-styled-components

---
+++ src/templates/App/index.jsx
@@ -0,0 +1,13 @@
+import * as Styled from "./styles";
+
+function Home() {
+  return (
+    <div>
+      <Styled.Wrapper background="blue">
+        <h1>Hello World!</h1>
+      </Styled.Wrapper>
+    </div>
+  );
+}
+
+export default Home;
