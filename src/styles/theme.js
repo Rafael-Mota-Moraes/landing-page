5836025919fa0b73221ff677477a9939126a8bdb
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-10-01 15:23:16 -0300

    configurando fonts

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,5 +3,11 @@ export const theme = {
     primaryColor: "#0a1128",
     secondaryColor: "#dc143c",
     white: "#fff"
+  },
+  font: {
+    family: {
+      default: "'Open Sans', sans-serif",
+      secondary: "'Montserrat', sans-serif"
+    }
   }
 };
